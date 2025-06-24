73dcbfa095a17fe6e9d344ae2b3c8319853a767c
Yevgen Panych <99422924+iseaman89@users.noreply.github.com>
2025-06-24 15:26:57 +0200

    Delete TaskManagementClient directory

--- TaskManagementClient/Services/Authentication/IAuthenticationService.cs
+++
@@ -1,9 +0,0 @@
-using TaskManagementClient.Services.Base;
-
-namespace TaskManagementClient.Services.Authentication;
-
-public interface IAuthenticationService
-{
-    Task<bool> AuthenticateAsync(LoginUserDto loginModel);
-    public Task Logout();
-}
--- TaskManagementClient/Services/Base/Response.cs
+++
@@ -1,9 +0,0 @@
-namespace TaskManagementClient.Services.Base;
-
-public class Response<T>
-{
-    public string Message { get; set; }
-    public string ValidationErrors { get; set; }
-    public bool Success { get; set; }
-    public T Data { get; set; }
-}
